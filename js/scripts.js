5ca38367ef5cce45b4a4159659b75d12b95b833d
Verdus <verdus@eagles.ewu.edu>
2020-05-02 16:04:00 -0700

    V.02
    +updated code
    +toggle class for h3

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,8 @@
 // JavaScript Document
 $(document).ready(function() {
     // all custom jQuery will go here
-	function(event) {
-  event.preventDefault();
-  var topicheader = $(this).parent(".topicheader");
-  topicHeader.next(".topicbody").slideToggle("fast", function() {
-    if ($(this).css("display") == "none") {
-      topicHeader
-        .addClass("Open")
-        .next(".topicbody")
-        .removeAttr("style");
-    } else {
-      topicHeader
-        .removeClass("Open")
-        .next(".topicbody")
-        .removeAttr("style");
-    }
-  });
-}
+$( ".topicheader" ).click(function() {
+  $( this ).next( ".topicbody" ).toggleClass( "Open" );
+});
+
 });
