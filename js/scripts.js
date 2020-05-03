424e1684a4d2f7c986f68e9cb360b754c5e1a9b2
Verdus <verdus@eagles.ewu.edu>
2020-05-02 19:52:47 -0700

    v.01
    +began styling
    +added css classes to paragraphs inside divs

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 $(document).ready(function() {
     // all custom jQuery will go here
 $( ".topicheader" ).click(function() {
-  $( this ).next( ".topicbody" ).toggleClass( "Open" );
+  $( this ).next( ".topicbody" ).slideToggle( "fast" );
 });
 
 });
