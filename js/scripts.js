6a0f5d89cdc485230e3977803755d8b1564462a7
Verdus <verdus@eagles.ewu.edu>
2020-05-02 21:39:33 -0700

    V1.0
    +fixed nav dropdown
    +updated colors
    +added chevrons

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,13 @@ $(document).ready(function() {
 $( ".topicheader" ).click(function() {
   $( this ).next( ".topicbody" ).slideToggle( "fast" );
 });
-
+$('nav li ul').hide().removeClass('hidden');
+$('nav li').hover(
+  function () {
+    $('ul', this).stop().slideDown(100);
+  },
+  function () {
+    $('ul', this).stop().slideUp(100);
+  }
+);
 });
